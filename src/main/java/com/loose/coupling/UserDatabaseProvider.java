739acd8af5243e0a762acf0cd9e82c76c9ab8b4c
Nitesh-loop <nitesh166k@gmail.com>
2025-02-02 18:20:02 +0530

    Loose Coupling

--- src/main/java/com/loose/coupling/UserDatabaseProvider.java
+++ src/main/java/com/loose/coupling/UserDatabaseProvider.java
@@ -1,10 +1,10 @@
-package com.tight.coupling;
+package com.loose.coupling;
 
 //A - MySQL, PostgreSQL
 //B - Web Service, MongoDB
 
-public class UserDatabase {
-
+public class UserDatabaseProvider implements UserDataProvider{
+    @Override
     public String getUserDetails(){
         // Directly access database here
         return  "User Details from Database";
