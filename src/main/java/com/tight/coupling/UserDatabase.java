94f4b69009097c53ddffb565e2abdb5ec5bc0c1e
Nitesh-loop <nitesh166k@gmail.com>
2025-02-02 18:06:51 +0530

    Tight Coupling

--- src/main/java/com/tight/coupling/UserDatabase.java
+++ src/main/java/com/tight/coupling/UserDatabase.java
@@ -1,4 +1,9 @@
 package com.tight.coupling;
 
 public class UserDatabase {
+
+    public String getUserDetails(){
+        // Directly access database here
+        return  "User Details from Database";
+    }
 }
